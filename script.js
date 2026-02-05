61ed7a16497f7b0c6df195ece5db35eba284aa65
Atharva Koli <121417032+Atharva-Koli@users.noreply.github.com>
2026-02-05 17:53:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+function strip(bandName){
+	return bandName.replace(/^(a |an |the )/i,'').trim()
+}
+const sortedbands= bands,sort((a,b)=>{
+	return strip(a).localecompare(strip(b));
+})
+
+const ul = dociment.getElementbyId('band')
+sortedbands.forEach(band=>{
+	const li = document.createElement('li')
+	li.textContent= band;
+	ul. appendChild(li;)
+})
+
+
+
+
